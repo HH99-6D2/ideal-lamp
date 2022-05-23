fda6a1313f25df3a4311584c9b6f8380b1ef26e1
junehan <junehan.dev@gmail.com>
2022-05-23 15:00:33 +0900

    [FIX] Tap testing 함수 수정

--- test/user.test.js
+++ test/user.test.js
@@ -3,20 +3,21 @@ const build = require("../src/app");
 const prefix = "/users";
 
 test(`Test on"${prefix}/" route GET`, async (t) => {
-	const app = build();
-	const users = require("../src/user/users");
-
+	t.beforeEach(()=> {
+		app = build();
+		users = require("../src/user/users");
+	})
 	t.test(`LIST USER - "${prefix}/", expect 200`, async (t) => {
 		const res = await app.inject({
 			method: "GET",
 			url: "/users",
 		});
-		t.strictEqual(res.statusCode, 200, "return a status code of 200");
-		t.strictEqual(
+		t.equal(res.statusCode, 200, "return a status code of 200");
+		t.equal(
 			res.headers["content-type"],
 			"application/json; charset=utf-8",
 		);
-		t.deepEqual(JSON.parse(res.payload), users);
+		t.same(JSON.parse(res.payload), users);
 	});
 
 	t.test(`GET USER - "${prefix}/:id", expect 200`, async (t) => {
@@ -24,12 +25,12 @@ test(`Test on"${prefix}/" route GET`, async (t) => {
 			method: "GET",
 			url: "/users/1",
 		});
-		t.strictEqual(res.statusCode, 200, "return a status code of 200");
-		t.strictEqual(
+		t.equal(res.statusCode, 200, "return a status code of 200");
+		t.equal(
 			res.headers["content-type"],
 			"application/json; charset=utf-8",
 		);
-		t.deepEqual(JSON.parse(res.payload), users[0]);
+		t.same(JSON.parse(res.payload), users[0]);
 	});
 
 	t.test(`GET USER - "${prefix}/:id", expect 404`, async (t) => {
@@ -37,8 +38,8 @@ test(`Test on"${prefix}/" route GET`, async (t) => {
 			method: "GET",
 			url: `/users/${users.length + 2}`,
 		});
-		t.strictEqual(res.statusCode, 404, "return a status code of 404");
-		t.strictEqual(res.headers["content-type"], "text/plain; charset=utf-8");
-		t.strictEqual(res.payload, `User ${users.length + 2} was not found`);
+		t.equal(res.statusCode, 404, "return a status code of 404");
+		t.equal(res.headers["content-type"], "text/plain; charset=utf-8");
+		t.equal(res.payload, `User ${users.length + 2} was not found`);
 	});
 });
